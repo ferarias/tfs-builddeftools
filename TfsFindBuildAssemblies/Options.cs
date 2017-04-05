d9860e3f0e56542372c1812d27524eac2804ec13
Fernando Arias Marqués <fernando.arias@logitravelgroup.com>
2017-04-05 17:45:02 +0200

    Improve the verbosity of TfsFindBuildAssemblies

--- TfsFindBuildAssemblies/Options.cs
+++ TfsFindBuildAssemblies/Options.cs
@@ -13,10 +13,10 @@ namespace TfsFindBuildAssemblies
         [Option('a', "assemblies", Required = true, HelpText = "Assemblies location.")]
         public string AssembliesLocation { get; set; }
 
-        [OptionArray('b', "build", Required = false, HelpText = "Build definition names (wildcards accepted).")]
+        [OptionArray('b', "build", Required = true, HelpText = "Build definition names (wildcards accepted). E.g.: *.Main")]
         public string[] BuildName { get; set; }
 
-        [Option('o', "out", Required = false, HelpText = "Output file.")]
+        [Option('o', "out", Required = true, HelpText = "Output file. E.g.: D:\\TEMP\\out.csv")]
         public string OutFile { get; set; }
 
         [HelpOption]
@@ -30,7 +30,7 @@ namespace TfsFindBuildAssemblies
                 AddDashesToOption = true
             };
             help.AddPreOptionsLine("License: As is");
-            help.AddPreOptionsLine("Usage: TfsFindBuildAssemblies.exe -c http://logpmtfs01v:8080/tfs/Logitravel http://logpmtfs01v:8080/tfs/Sales [options]");
+            help.AddPreOptionsLine("Usage: TfsFindBuildAssemblies.exe -c http://logpmtfs01v:8080/tfs/Logitravel http://logpmtfs01v:8080/tfs/Sales -a \\\\acmecatfs01v\\SharedTFS\\Assemblies -b *.Main -o D:\\TEMP\\out.csv");
             help.AddOptions(this);
             return help;
         }
